eac2c7ee41565a200aa4bd39b99d3c9f5ffa8e0a
mcdetorres <mmdetorres@gbox.adnu.edu.ph>
2026-07-12 22:27:47 +0800

    Add Dockerfile for Render

--- config/database.php
+++ config/database.php
@@ -1,16 +1,11 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "adnu_prints";
+$host = "sql111.infinityfree.com";
+$user = "if0_42386269";
+$password = "NI3MOWvVqrl";
+$database = "if0_42386269_adnuprints";
 
-$conn = mysqli_connect(
-    $host,
-    $user,
-    $password,
-    $database
-);
+$conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
