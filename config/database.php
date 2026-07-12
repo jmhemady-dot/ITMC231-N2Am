806171176167bfa0db643a6429c14e0bae049d50
mcdetorres <mmdetorres@gbox.adnu.edu.ph>
2026-07-12 23:08:39 +0800

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
 
-$host = "sql111.infinityfree.com";
-$user = "if0_42386269";
-$password = "NI3MOWvVqrl";
-$database = "if0_42386269_adnuprints";
+$host = "sql12.freesqldatabase.com";
+$user = "sql12832861";
+$password = "JcHRkhzQgl";
+$database = "sql12832861";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
